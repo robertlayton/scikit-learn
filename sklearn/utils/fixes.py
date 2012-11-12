8d4f9d6d60cb2335810f71d32b5fa8339a6b531e
bob <bob@legend.(none)>
2012-11-12 22:56:39 +1100

    Moved tupling earlier to account for all uses

--- sklearn/utils/fixes.py
+++ sklearn/utils/fixes.py
@@ -107,8 +107,9 @@ for x in np.__version__.split('.'):
     except ValueError:
         # x may be of the form dev-1ea1592
         np_version.append(x)
+np_version = tuple(np_version)
 
-if tuple(np_version[:2]) < (1, 5):
+if np_version[:2] < (1, 5):
     unique = _unique
 else:
     unique = np.unique
